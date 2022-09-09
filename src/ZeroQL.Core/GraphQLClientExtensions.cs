bb79524d8e6eca68b8c907c21d0a5380b821554d
stanislav <silin.stanislav@outlook.com>
2022-09-09 16:12:31 +0300

    Move extensions into global namespace to avoid import ZeroQL namespace.

--- src/ZeroQL.Core/GraphQLClientExtensions.cs
+++ src/ZeroQL.Core/GraphQLClientExtensions.cs
@@ -2,8 +2,7 @@ using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
-
-namespace ZeroQL;
+using ZeroQL;
 
 [SuppressMessage("ReSharper", "UnusedParameter.Global")]
 public static class GraphQLClientExtensions
