eb6a4af0da32f0918b956a20bf1d621567781166
stanislav <silin.stanislav@outlook.com>
2023-06-01 20:18:18 +0300

    Add test execute for netstandard.

--- src/TestApp/TestStandardLibrary/Client.cs
+++ src/TestApp/TestStandardLibrary/Client.cs
@@ -1,7 +1,18 @@
+using System;
+using System.Threading.Tasks;
+using GraphQL.TestServer;
+
 namespace TestStandardLibrary
 {
     public class Client
     {
-        
+        public async Task Execute()
+        {
+            var httpClient = new System.Net.Http.HttpClient();
+            httpClient.BaseAddress = new Uri("http://localhost:10000/graphql");
+            
+            var graphQLClient = new TestServerClient(httpClient);
+            var response = await graphQLClient.Query(o => o.Me(o => o.FirstName));
+        }
     }
 }
