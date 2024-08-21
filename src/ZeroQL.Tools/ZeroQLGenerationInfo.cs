eedb40cdf5c9256dbcd529278b66550fed942efb
stanislav <silin.stanislav@outlook.com>
2024-08-21 12:28:22 +0300

    Mark "__" properties as internal to avoid issues once removed.

--- src/ZeroQL.Tools/ZeroQLGenerationInfo.cs
+++ src/ZeroQL.Tools/ZeroQLGenerationInfo.cs
@@ -15,6 +15,8 @@ public class ZeroQLGenerationInfo
 
     public static string EditorBrowsableAttribute => "global::System.ComponentModel.EditorBrowsable";
 
+    public static string ObsoleteAttribute => "global::System.ObsoleteAttribute";
+
     public static string EditorBrowsableNeverParameter => "global::System.ComponentModel.EditorBrowsableState.Never";
 
     public static string JsonIgnoreAttribute => "JsonIgnore";
