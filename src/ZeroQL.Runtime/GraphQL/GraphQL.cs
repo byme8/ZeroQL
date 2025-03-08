380d2740a812d723d75ca749de17c0a29e98af9f
stanislav <silin.stanislav@outlook.com>
2025-03-08 07:52:05 +0200

    Deprecate request syntax.

--- src/ZeroQL.Runtime/GraphQL/GraphQL.cs
+++ src/ZeroQL.Runtime/GraphQL/GraphQL.cs
@@ -1,6 +1,10 @@
 ﻿// ReSharper disable once CheckNamespace
+
+using System;
+
 namespace ZeroQL;
 
+[Obsolete("Request syntax is deprecated because it is not compatible with AOT runtimes. It will be removed in the future releases.")]
 public abstract record GraphQL<TQuery, TResult>
 {
     public abstract TResult Execute(TQuery query);
